65ff785974ece001a3348abc91f402727bd2185d
Madhawa <t.madhawa.k@gmail.com>
2023-07-29 23:59:25 +0530

    2nd

--- prject test3.py
+++ prject test3.py
@@ -2,9 +2,10 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-image = cv2.imread("C:\\Users\\Madhawa\\Desktop\\work\\python\\Project\\image8.jpg",0)
+image = cv2.imread("C:\\Users\\Madhawa\\Desktop\\work\\python\\Project\\image1.jpg",0)
 green = cv2.imread("C:\\Users\\Madhawa\\Desktop\\work\\python\\Project\\green1.jpg",1)
 
+kernal = cv2.getStructuringElement(cv2.MORPH_RECT,(5,5))
 #gus1 = cv2.GaussianBlur(image,(61,61),0)
 gus1 = cv2.medianBlur(image,15)
 sharp1 = cv2.addWeighted(image,2,gus1,-1,0)
@@ -12,6 +13,7 @@ sharp1 = cv2.addWeighted(image,2,gus1,-1,0)
 gus2 = cv2.GaussianBlur(image,(19,19),0)
 
 mask = cv2.subtract(image,gus2)
+
 #mask = cv2.medianBlur(mask,5)
 height, width = mask.shape
 green = cv2.resize(cv2.cvtColor(green,cv2.COLOR_BGR2RGB),(width,height))
